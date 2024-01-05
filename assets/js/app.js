fefeecc96df58c5bd93fb282c3d1d66e564478c5
MatviyVovchuk <doc25900@gmail.com>
2024-01-05 17:37:11 +0200

    slider start

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,25 @@
+// MENU BURGER
+
 document.querySelector('.menu-burger').addEventListener('click', function(){
     this.classList.toggle('active');
     document.querySelector('.menu-nav-ul').classList.toggle('open');
-})
+});
+
+// SLIDER
+
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        dots:false,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:1000,
+        easing:'_linear',
+        infinite:true,
+        initialSlide:0,
+        autoplay:true,
+        autoplaySpeed:1000,
+
+    });
+});
