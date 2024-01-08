ada767f6bf6dd546bc0f67fc88b0888b0c4d370a
MatviyVovchuk <doc25900@gmail.com>
2024-01-08 17:28:17 +0200

    slick slier styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,33 @@
-// MENU BURGER
+/* MENU BURGER */
 
 document.querySelector('.menu-burger').addEventListener('click', function(){
     this.classList.toggle('active');
     document.querySelector('.menu-nav-ul').classList.toggle('open');
 });
 
-// SLIDER
+/* SLIDER */
 
-$(document).ready(function(){
-    $('.slider').slick({
-        arrows:true,
-        dots:false,
-        adaptiveHeight:true,
-        slidesToShow:1,
-        slidesToScroll:1,
-        speed:1000,
-        easing:'_linear',
-        infinite:true,
-        initialSlide:0,
-        autoplay:true,
-        autoplaySpeed:1000,
+$('.slider').slick({
+    arrows: false,
+    dots: false,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+    easing: '_linear',
+    infinite: true,
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    // fade: true,
+    cssEase: 'linear',
+});
 
-    });
+// Button click functions
+$(".slider-btn-prew").click(function () {
+    $(".slider").slick("slickPrev");
+});
+
+$(".slider-btn-next").click(function () {
+    $(".slider").slick("slickNext");
 });
