b2a8bf71130c6d1737c95405fc30df90c27f1f35
MatviyVovchuk <doc25900@gmail.com>
2024-01-11 17:51:33 +0200

    finish content and footer & adaptive 1000 and 800

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,48 @@
 /* MENU BURGER */
 
 document.querySelector(".menu-burger").addEventListener("click", function () {
-    this.classList.toggle("active");
-    document.querySelector(".menu-nav-ul").classList.toggle("open");
+  this.classList.toggle("active");
+  document.querySelector(".menu-nav-ul").classList.toggle("open");
 });
 
 /* SLIDER */
 
-$('.slider').slick({
-    arrows: false,
-    dots: false,
-    adaptiveHeight: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 1000,
-    easing: '_linear',
-    infinite: true,
-    initialSlide: 0,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    fade: true,
-    cssEase: 'linear',
+$(".slider").slick({
+  arrows: false,
+  dots: false,
+  adaptiveHeight: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 1000,
+  easing: "_linear",
+  infinite: true,
+  initialSlide: 0,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  fade: true,
+  cssEase: "linear",
 });
 
 // Button slider navigation
 $(".slider-btn-prew").click(function () {
-    $(".slider").slick("slickPrev");
+  $(".slider").slick("slickPrev");
 });
 
 $(".slider-btn-next").click(function () {
-    $(".slider").slick("slickNext");
+  $(".slider").slick("slickNext");
 });
 
 /* SUBCATEGORY DROPDOWN */
 
-$(document).ready(function() {
-    // Hide subcategory dropdowns by default
-    $('.subcategory-dropdown').hide();
+$(document).ready(function () {
+  // Hide subcategory dropdowns by default
+  $(".subcategory-dropdown").hide();
 
-    // Add click event to category items
-    $('.category-item').click(function(event) {
-        event.preventDefault();
+  // Add click event to category items
+  $(".category-item").click(function (event) {
+    event.preventDefault();
 
-        // Toggle the display of the subcategory-dropdown with sliding animation
-        $(this).find('.subcategory-dropdown').slideToggle();
-    });
+    // Toggle the display of the subcategory-dropdown with sliding animation
+    $(this).find(".subcategory-dropdown").slideToggle();
+  });
 });
